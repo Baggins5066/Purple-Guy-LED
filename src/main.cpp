045582b4c1d8f14db10c6fd3d38bac61bc9da836
Baggins5066 <little0jasper@gmail.com>
2024-03-14 10:18:49 -0400

    Removed jpearman LEDs Added solid purple LEDs in main() with Sylib

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,14 @@ using namespace vex;
 
 competition Competition;
 
-auto myLights = sylib::Addrled(20, 1, 64);
-// addressable_led addrled(vex::triport(PORT20).A, 60 );
-// digital_out PortA = digital_out(Brain.ThreeWirePort.A);
+auto myLights = sylib::Addrled(22, 6, 64);
+
+void initialize() {
+  sylib::initialize();
+}
 
 void pre_auton(void) {
   vexcodeInit();
-  sylib::initialize();
 }
 
 void autonomous(void) {
@@ -19,16 +20,12 @@ void autonomous(void) {
 }
 
 void usercontrol(void) {
-  Brain.Screen.setFillColor(purple); //Makes the background of the brain display black
-  Brain.Screen.drawRectangle(0, 0, 480, 240); // Define the rectangle's size to cover the entire screen
-  Brain.Screen.setFont(mono30);
-  Brain.Screen.setPenColor(purple); //Defines the text color
-
-  sylib::initialize();
-  myLights.turn_on();
-  // PortA.set(true);
-  myLights.set_all(purple);
-  // Brain.Screen.printAt(50, 150, "A: %f", PortA.value());
+  //Brain Text
+    Brain.Screen.setFillColor(purple); //Makes the background of the brain display black
+    Brain.Screen.drawRectangle(0, 0, 480, 240); // Define the rectangle's size to cover the entire screen
+    Brain.Screen.setFont(mono30);
+    Brain.Screen.setPenColor(purple); //Defines the text color
+    Brain.Screen.printAt(50, 150, "ULTRAVIOLET");
   while (true) {
     //Rainbow Lighting
       // myLights.gradient(0xFF0000, 0xFF0005, 0, 0, false, true);
@@ -36,11 +33,13 @@ void usercontrol(void) {
       // std::uint32_t clock = sylib::millis();
       // while (true) {
       //   sylib::delay_until(&clock, 10);
-      // }
     wait(20, msec); 
   }
 }
 
 int main() {
+  sylib::initialize();
+  myLights.turn_on();
+  myLights.set_all(purple);
   vexcodeInit();
 }
