6ef3402ecdd78d10f3c41a526c54a3f6447a3417
Baggins5066 <little0jasper@gmail.com>
2024-03-20 17:30:43 -0400

    Added two more LED strips Renamed idleLEDs() Added more code from original Purple_Guy code LEDs now work by setting the first pixel a color and then cycling it through the whole strip LEDs change color when the joystick is moved

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,67 @@ using namespace vex;
 
 competition Competition;
 
-auto LEDs = sylib::Addrled(22, 6, 64); //Creates LEDs sylib object
+//LED definitions
+  auto coreLEDs = sylib::Addrled(22,6,64); //Creates LEDs sylib object
+  auto leftDriveLEDs = sylib::Addrled(16, 1, 55); //Creates LEDs sylib object
+  auto rightDriveLEDs = sylib::Addrled(17,1,55); //Creates LEDs sylib object
 
-void idleLEDs() {
-  LEDs.gradient(purple, white,64,0); //Start color, End color, Fade width, Start position
-  LEDs.cycle(*LEDs,10,0);
-  // LEDs.set_pixel(red,0);
-  // LEDs.set_pixel(green,32);
-  // LEDs.set_pixel(blue,63);
+void UVGradient() {
+  leftDriveLEDs.gradient(purple, white,64,0);
+  leftDriveLEDs.cycle(*leftDriveLEDs,10,0);
 }
 
 void shootTriball() {
   Cata.spinFor(-180,deg,false);
-  LEDs.set_all(orange);
-  wait(0.5,sec);
-  idleLEDs();
+  leftDriveLEDs.set_all(orange);
+  rightDriveLEDs.set_all(orange);
+  coreLEDs.set_all(orange);
+}
+bool desiredLiftPosition = false;
+
+void toggleLeftWing() {
+  if (leftWing.value() == false) {
+    leftWing.set(true);
+  }
+  else {
+    leftWing.set(false);
+  }
+}
+
+void toggleRightWing() {
+  if (rightWing.value() == false) {
+    rightWing.set(true);
+    
+  }
+  else {
+    rightWing.set(false);
+  }
+}
+
+void toggleBarrierHanger() {
+  if (barrierHanger.value() == false) {
+    barrierHanger.set(true);
+  }
+  else {
+    barrierHanger.set(false);
+  }
+}
+
+void toggleLift() {
+  if (Lift.value() == false) {
+    Lift.set(true);
+    desiredLiftPosition = true;
+  }
+  else {
+    Lift.set(false);
+    desiredLiftPosition = false;
+  }
+}
+
+void retractAllWings() {
+  barrierHanger.set(false);
+  leftWing.set(false);
+  rightWing.set(false);
 }
 
 void initialize() {
@@ -39,11 +85,37 @@ void usercontrol(void) {
     Brain.Screen.setFont(mono30);
     Brain.Screen.setPenColor(purple); //Defines the text color
     Brain.Screen.printAt(50, 150, "ULTRAVIOLET");
-  //LEDs
-    idleLEDs();
+  color coreColor = purple;
+  color LDColor = purple;
+  color RDColor = purple;
+  Drivetrain.setStopping(brake);
+  Cata.setStopping(coast);
+  Cata.setVelocity(100, percent);
   while (true) {
+    coreColor = cataSensor.color();
     Controller1.ButtonL1.pressed(shootTriball);
-    wait(20, msec); 
+    leftDriveLEDs.set_pixel(LDColor,0);
+    leftDriveLEDs.rotate(1,false);
+    rightDriveLEDs.set_pixel(RDColor,0);
+    rightDriveLEDs.rotate(1,false);
+    coreLEDs.set_pixel(coreColor,0);
+    coreLEDs.rotate(1,false);
+    if (Controller1.Axis1.position() > 25) {
+      RDColor = white;
+    }
+    else {
+      RDColor = coreColor;
+    }
+    if (Controller1.Axis1.position() < -25) {
+      LDColor = white;
+    }
+    else {
+      LDColor = coreColor;
+    }
+
+    // std::uint32_t clock = sylib::millis();
+    // sylib::delay_until(&clock, 10);
+    wait(10, msec); 
   }
 }
 
